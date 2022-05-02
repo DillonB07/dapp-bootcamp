5999698ed1a899b8014d41a61fa06c8de8687051
DillonB07 <dillonbarnes07@gmail.com>
2022-05-02 18:52:23 +0100

    Create orders

--- src/store/actions.js
+++ src/store/actions.js
@@ -142,3 +142,56 @@ export function tokenWithdrawAmountChanged(amount) {
     amount,
   };
 }
+
+//* Buy Orders
+
+export function buyOrderAmountChanged(amount) {
+  return {
+    type: "BUY_ORDER_AMOUNT_CHANGED",
+    amount,
+  };
+}
+
+export function buyOrderPriceChanged(price) {
+  return {
+    type: "BUY_ORDER_PRICE_CHANGED",
+    price,
+  };
+}
+
+export function buyOrderMaking() {
+  return {
+    type: "BUY_ORDER_MAKING",
+  };
+}
+
+//* Generic Orders
+
+export function orderMade(order) {
+  return {
+    type: "ORDER_MADE",
+    order,
+  };
+}
+
+//* Sell Orders
+
+export function sellOrderAmountChanged(amount) {
+  return {
+    type: "SELL_ORDER_AMOUNT_CHANGED",
+    amount,
+  };
+}
+
+export function sellOrderPriceChanged(price) {
+  return {
+    type: "SELL_ORDER_PRICE_CHANGED",
+    price,
+  };
+}
+
+export function sellOrderMaking() {
+  return {
+    type: "SELL_ORDER_MAKING",
+  };
+}
--- src/store/interactions.js
+++ src/store/interactions.js
@@ -17,6 +17,9 @@ import {
   tokenBalanceLoaded,
   etherBalanceLoaded,
   balancesLoading,
+  buyOrderMaking,
+  sellOrderMaking,
+  orderMade,
 } from "./actions";
 import Token from "../abis/Token.json";
 import Exchange from "../abis/Exchange.json";
@@ -114,6 +117,10 @@ export const subscribeToEvents = async (exchange, dispatch) => {
 
   exchange.events.Withdraw({}, (error, event) => {
     dispatch(balancesLoaded());
+  });
+
+  exchange.events.Order({}, (error, event) => {
+    dispatch(orderMade(event.returnValues));
   });
 };
 
@@ -244,6 +251,62 @@ export const withdrawToken = (
     .send({ from: account })
     .on("transactionHash", (hash) => {
       dispatch(balancesLoading());
+    })
+    .on("error", (error) => {
+      console.log(error);
+      window.alert("There was an error!", error);
+    });
+};
+
+export const makeBuyOrder = (
+  dispatch,
+  exchange,
+  token,
+  web3,
+  order,
+  account
+) => {
+  const tokenGet = token.options.address;
+  const amountGet = web3.utils.toWei(order.amount, "ether");
+  const tokenGive = ETHER_ADDRESS;
+  const amountGive = web3.utils.toWei(
+    (order.amount * order.price).toString(),
+    "ether"
+  );
+
+  exchange.methods
+    .makeOrder(tokenGet, amountGet, tokenGive, amountGive)
+    .send({ from: account })
+    .on("transactionHash", (hash) => {
+      dispatch(buyOrderMaking());
+    })
+    .on("error", (error) => {
+      console.log(error);
+      window.alert("There was an error!", error);
+    });
+};
+
+export const makeSellOrder = (
+  dispatch,
+  exchange,
+  token,
+  web3,
+  order,
+  account
+) => {
+  const tokenGet = ETHER_ADDRESS;
+  const amountGet = web3.utils.toWei(
+    (order.amount * order.price).toString(),
+    "ether"
+  );
+  const tokenGive = token.options.address;
+  const amountGive = web3.utils.toWei(order.amount, "ether");
+
+  exchange.methods
+    .makeOrder(tokenGet, amountGet, tokenGive, amountGive)
+    .send({ from: account })
+    .on("transactionHash", (hash) => {
+      dispatch(sellOrderMaking());
     })
     .on("error", (error) => {
       console.log(error);
